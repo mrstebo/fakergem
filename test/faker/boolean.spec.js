acbede05d283fe45a367ac9037601249c1d0e76e
Steven Atkinson <mrstebouk@gmail.com>
2017-04-01 20:16:16 +0100

    Implemented the Boolean faker module with tests.

---
+++ test/faker/boolean.spec.js
@@ -0,0 +1,21 @@
+import {expect} from 'chai';
+import * as Boolean from '../../lib/faker/boolean';
+
+describe('Boolean', () => {
+  describe('#boolean', () => {
+    it('should return a boolean', () => {
+      expect(Boolean.boolean()).to.be.a('boolean');
+    });
+    it('should return true or false', () => {
+      [...Array(100).keys()].map(_ => {
+        expect(Boolean.boolean()).to.be.oneOf([true, false]);
+      });
+    });
+    it('should always return false when trueBias is 0', () => {
+      expect(Boolean.boolean(0)).to.be.false;
+    });
+    it('should always return true when trueBias is 1', () => {
+      expect(Boolean.boolean(1)).to.be.true;
+    });
+  });
+});
