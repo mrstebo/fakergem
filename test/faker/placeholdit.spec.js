26af8ca9dc0f93a7732f56c1e9b25b51b74a433b
Steven Atkinson <mrstebouk@gmail.com>
2017-04-01 22:39:53 +0100

    Added extra parameter validation with tests.

--- test/faker/placeholdit.spec.js
+++ test/faker/placeholdit.spec.js
@@ -15,21 +15,46 @@ describe('Placeholdit', () => {
       });
     });
     it('should return URL with specified background color', () => {
-      ['ff0000', '00ff00', '0000ff', '000000', 'ffffff'].forEach(color => {
+      ['000000', 'ffffff', '000', 'fff'].forEach(color => {
         expect(Placeholdit.image('300x300', 'png', color)).to.equal(`https://placehold.it/300x300.png/${color}`);
       });
     });
     it('should return URL with specified text color', () => {
-      ['ff0000', '00ff00', '0000ff', '000000', 'ffffff'].forEach(color => {
-        expect(Placeholdit.image('300x300', 'png', 'ffffff', color)).to.equal(`https://placehold.it/300x300.png/ffffff/${color}`);
+      ['000000', 'ffffff', '000', 'fff'].forEach(color => {
+        expect(Placeholdit.image('300x300', 'png', 'fff', color)).to.equal(`https://placehold.it/300x300.png/fff/${color}`);
       });
     });
     it('should return URL with specified text', () => {
-      expect(Placeholdit.image('300x300', 'png', 'ffffff', 'ffffff', 'test')).to.equal('https://placehold.it/300x300.png/ffffff/ffffff?text=test');
+      expect(Placeholdit.image('300x300', 'png', 'fff', 'fff', 'test')).to.equal('https://placehold.it/300x300.png/fff/fff?text=test');
     });
     it('should throw if size is not in a valid format', () => {
       ['100', 'abc', '2x2x2'].forEach(size => {
         expect(() => Placeholdit.image(size)).to.throw();
+      });
+    });
+    it('should throw if format is not supported', () => {
+      ['txt', 'pdf', 'dat', 'x'].forEach(format => {
+        expect(() => Placeholdit.image('300x300', format)).to.throw();
+      });
+    });
+    it('should throw if background color contains #', () => {
+      ['#000', '#fff', '#000000', '#ffffff'].forEach(color => {
+        expect(() => Placeholdit.image('300x300', 'png', color)).to.throw();
+      });
+    });
+    it('should throw if background color is invalid', () => {
+      ['ggg', '1fg', 'xxa'].forEach(color => {
+        expect(() => Placeholdit.image('300x300', 'png', color)).to.throw();
+      });
+    });
+    it('should throw if text color contains #', () => {
+      ['#000', '#fff', '#000000', '#ffffff'].forEach(color => {
+        expect(() => Placeholdit.image('300x300', 'png', 'fff', color)).to.throw();
+      });
+    });
+    it('should throw if text color is invalid', () => {
+      ['ggg', '1fg', 'xxa'].forEach(color => {
+        expect(() => Placeholdit.image('300x300', 'png', 'fff', color)).to.throw();
       });
     });
   });
