d76df9c06931d4708d237e2add6eb55d3f646afc
Steven Atkinson <mrstebouk@gmail.com>
2017-04-03 07:01:49 +0100

    Added a leftPad function that is used by hexColor so we always return 6 characters.

--- src/faker/color.js
+++ src/faker/color.js
@@ -1,9 +1,14 @@
 export function hexColor() {
-  return `#${(Math.floor(Math.random() * 0xffffff)).toString(16)}`;
+  return `#${leftPad(Math.floor(Math.random() * 0xffffff).toString(16))}`;
 }
 
 export function rgbColor() {
   return [...Array(3).keys()].map(_ => {
     return Math.floor(Math.random() * 255);
   });
+}
+
+function leftPad(text, length, paddingCharacter) {
+  let padding = [...Array(length)].map(_ => paddingCharacter).join('');
+  return (padding + text).slice(-length);
 }
