60aa538deef7e6fd61b8a1a9cebc84e52bfa31af
Steven Atkinson <mrstebouk@gmail.com>
2017-04-05 20:20:31 +0100

    Added more tests for betweenExcept, forward and backward functions.

--- test/faker/date.spec.js
+++ test/faker/date.spec.js
@@ -34,7 +34,22 @@ describe('#Date', () => {
     it('should handle string dates', () => {
       const from = new Date(2017, 0, 1);
       const to = new Date(2017, 0, 10);
-      expect(DateFaker.betweenExcept('2017-01-01', '2017-01-10', '2017-01-04')).to.be.within(from, to);
+      const except = new Date(2017, 0, 4);
+      expect( DateFaker.betweenExcept('2017-01-01', '2017-01-10', '2017-01-04'))
+        .to.be.within(from, to)
+        .and.not.equal(except);
+    });
+
+    it('should return a date in between two dates except for the excepted date', () => {
+      const from = new Date(2017, 0, 1);
+      const to = new Date(2017, 0, 10);
+      const except = new Date(2017, 0, 5);
+
+      [...Array(100).keys()].forEach(_ => {
+        expect(DateFaker.betweenExcept(from, to, except))
+          .to.be.within(from, to)
+          .and.not.equal(except);
+      });
     });
   });
 
@@ -42,11 +57,39 @@ describe('#Date', () => {
     it('should return a Date', () => {
       expect(DateFaker.forward(10)).to.be.a('Date');
     });
+
+    it('should return a date in the future', () => {
+      expect(DateFaker.forward(10)).to.be.above(new Date());
+    });
+
+    it('should return a date no greater than the specified number of days', () => {
+      const max = new Date();
+      max.setDate(max.getDate() + 10);
+      max.setHours(0, 0, 0, 0);
+
+      [...Array(100).keys()].forEach(_ => {
+        expect(DateFaker.forward(10)).to.be.at.most(max);
+      });
+    });
   });
 
   describe('#backward', () => {
     it('should return a Date', () => {
       expect(DateFaker.backward(10)).to.be.a('Date');
+    });
+
+    it('should return a date in the past', () => {
+      expect(DateFaker.backward(10)).to.be.below(new Date());
+    });
+
+    it('should return a date no less than the specified number of days', () => {
+      const min = new Date();
+      min.setDate(min.getDate() - 10);
+      min.setHours(0, 0, 0, 0);
+
+      [...Array(100).keys()].forEach(_ => {
+        expect(DateFaker.backward(10)).to.be.at.least(min);
+      });
     });
   });
 
