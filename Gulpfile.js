18a305eff26d3b434e96d28d92b0c3f5e2b1cd38
Steven Atkinson <mrstebouk@gmail.com>
2017-04-05 21:37:22 +0100

    Added gulp task to run mocha tests.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const mocha = require('gulp-mocha');
 
 gulp.task('build', () => {
   return gulp.src('src/**/*.js')
@@ -7,4 +8,14 @@ gulp.task('build', () => {
       presets: ['es2015']
     }))
     .pipe(gulp.dest('lib'));
+});
+
+gulp.task('test', () => {
+  return gulp.src('test/**/*.spec.js', { read: false })
+    .pipe(mocha({
+      reporter: 'spec',
+      compilers: 'js:babel-core/register',
+      colors: true,
+      harmony: true
+    }));
 });
