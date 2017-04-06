aaa771c9a939a6ba1906f129e0c82a72320bc9f4
Steven Atkinson <stevenatkinson@ekmsystems.co.uk>
2017-04-06 12:47:51 +0100

    Added more passescalls of faker functions.

--- test/faker/name.spec.js
+++ test/faker/name.spec.js
@@ -9,9 +9,11 @@ describe('Name', () => {
     });
 
     it('should return a first and last name', () => {
-      const name = Name.name();
-      expect(name.split(' ')[0]).to.be.oneOf(data['firstNames']);
-      expect(name.split(' ')[1]).to.be.oneOf(data['lastNames']);
+      [...Array(100).keys()].forEach(_ => {
+        const name = Name.name();
+        expect(name.split(' ')[0]).to.be.oneOf(data['firstNames']);
+        expect(name.split(' ')[1]).to.be.oneOf(data['lastNames']);
+      });
     });
   });
 
@@ -21,10 +23,12 @@ describe('Name', () => {
     });
 
     it('should return two first names and a last name', () => {
-      const name = Name.nameWithMiddle();
-      expect(name.split(' ')[0]).to.be.oneOf(data['firstNames']);
-      expect(name.split(' ')[1]).to.be.oneOf(data['firstNames']);
-      expect(name.split(' ')[2]).to.be.oneOf(data['lastNames']);
+      [...Array(100).keys()].forEach(_ => {
+        const name = Name.nameWithMiddle();
+        expect(name.split(' ')[0]).to.be.oneOf(data['firstNames']);
+        expect(name.split(' ')[1]).to.be.oneOf(data['firstNames']);
+        expect(name.split(' ')[2]).to.be.oneOf(data['lastNames']);
+      });
     });
   });
 
@@ -34,7 +38,9 @@ describe('Name', () => {
     });
 
     it('should return a first name', () => {
-      expect(Name.firstName()).to.be.oneOf(data['firstNames']);
+      [...Array(100).keys()].forEach(_ => {
+        expect(Name.firstName()).to.be.oneOf(data['firstNames']);
+      });
     });
   });
 
@@ -44,7 +50,9 @@ describe('Name', () => {
     });
 
     it('should return a last name', () => {
-      expect(Name.lastName()).to.be.oneOf(data['lastNames']);
+      [...Array(100).keys()].forEach(_ => {
+        expect(Name.lastName()).to.be.oneOf(data['lastNames']);
+      });
     });
   });
 
@@ -54,7 +62,9 @@ describe('Name', () => {
     });
 
     it('should return a prefix', () => {
-      expect(Name.prefix()).to.be.oneOf(data['prefixes']);
+      [...Array(100).keys()].forEach(_ => {
+        expect(Name.prefix()).to.be.oneOf(data['prefixes']);
+      });
     });
   });
 
@@ -64,7 +74,9 @@ describe('Name', () => {
     });
 
     it('should return a suffix', () => {
-      expect(Name.suffix()).to.be.oneOf(data['suffixes']);
+      [...Array(100).keys()].forEach(_ => {
+        expect(Name.suffix()).to.be.oneOf(data['suffixes']);
+      });
     });
   });
 
@@ -74,10 +86,12 @@ describe('Name', () => {
     });
 
     it('should return a title descriptor, level and job', () => {
-      const title = Name.title();
-      expect(title.split(' ')[0]).to.be.oneOf(data['titles']['descriptor']);
-      expect(title.split(' ')[1]).to.be.oneOf(data['titles']['level']);
-      expect(title.split(' ')[2]).to.be.oneOf(data['titles']['job']);
+      [...Array(100).keys()].forEach(_ => {
+        const title = Name.title();
+        expect(title.split(' ')[0]).to.be.oneOf(data['titles']['descriptor']);
+        expect(title.split(' ')[1]).to.be.oneOf(data['titles']['level']);
+        expect(title.split(' ')[2]).to.be.oneOf(data['titles']['job']);
+      });
     });
   });
 });
