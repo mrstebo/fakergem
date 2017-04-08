d9a1c629a9004d545ee58f238302d75a3ca209ff
Steven Atkinson <mrstebouk@gmail.com>
2017-04-08 17:37:06 +0100

    Implemented the App faker with tests.

---
+++ src/faker/app.js
@@ -0,0 +1,38 @@
+const data = require('../../data/app.json');
+const nameData = require('../../data/name.json');
+const VERSION_FORMATS = [
+  '0.#.#',
+  '0.##',
+  '#.##',
+  '#.#',
+  '#.#.#'
+];
+
+export function name() {
+  return pickOneOf(data['names']);
+}
+
+export function version() {
+  const version = pickOneOf(VERSION_FORMATS);
+  return version.split('.').map(i => {
+    if (i =='#') {
+      return randomNumber();
+    }
+    return i;
+  }).join('.');
+}
+
+export function author() {
+  return [
+    pickOneOf(nameData['firstNames']),
+    pickOneOf(nameData['lastNames'])
+  ].join(' ');
+}
+
+function pickOneOf(collection) {
+  return collection[Math.floor(Math.random() * collection.length)];
+}
+
+function randomNumber() {
+  return Math.floor(Math.random() * 10);
+}
--- test/faker/app.spec.js
+++ test/faker/app.spec.js
@@ -1,3 +1,39 @@
 'use strict';
 const expect = require('chai').expect;
 const App = require('../../src/faker/app');
+const data = require('../../data/app.json');
+const nameData = require('../../data/name.json');
+
+describe('App', () => {
+  describe('#name', () => {
+    it('should return a string', () => {
+      expect(App.name()).to.be.a('string');
+    });
+
+    it('should return a name', () => {
+      expect(App.name()).to.be.oneOf(data['names']);
+    });
+  });
+
+  describe('#version', () => {
+    it('should return a string', () => {
+      expect(App.version()).to.be.a('string');
+    });
+
+    it('should return a version format', () => {
+      expect(App.version()).to.match(/(?:#|\d+)\.(?:[#]+|\d+)(?:\.(?:[#]+|\d+))?/);
+    });
+  });
+
+  describe('#author', () => {
+    it('should return a string', () => {
+      expect(App.author()).to.be.a('string');
+    });
+
+    it('should return a name', () => {
+      const author = App.author();
+      expect(author.split(' ')[0]).to.be.oneOf(nameData['firstNames']);
+      expect(author.split(' ')[1]).to.be.oneOf(nameData['lastNames']);
+    });
+  });
+});
