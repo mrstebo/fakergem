d44b29c7cd432ef2146d77138f8c76316262307d
Steven Atkinson <mrstebouk@gmail.com>
2017-04-25 19:53:29 +0100

    Using regex matches.

--- src/utils/date-helper.js
+++ src/utils/date-helper.js
@@ -2,24 +2,22 @@ const data = require('../../data/date.json');
 
 export function format(date, format) {
   return format
-    .replace('ss', pad(date.getSeconds(), 2))
-    .replace('s', date.getSeconds())
-    .replace('dd', pad(date.getDate(), 2))
-    .replace('d', date.getDate())
-    .replace('mm', pad(date.getMinutes(), 2))
-    .replace('m', date.getMinutes())
-    .replace('MMMM', data['months'][date.getMonth()])
-    .replace('MMM', data['months'][date.getMonth()].substring(0, 3))
-    .replace('MM', pad(date.getMonth() + 1, 2))
+    .replace(/ss/, pad(date.getSeconds(), 2))
+    .replace(/s/, date.getSeconds())
+    .replace(/dd/, pad(date.getDate(), 2))
+    .replace(/d/, date.getDate())
+    .replace(/mm/, pad(date.getMinutes(), 2))
+    .replace(/m/, date.getMinutes())
+    .replace(/MMMM/, data['months'][date.getMonth()])
+    .replace(/MMM/, data['months'][date.getMonth()].substring(0, 3))
+    .replace(/MM/, pad(date.getMonth() + 1, 2))
     .replace(/M(?![ao])/, date.getMonth() + 1)
-    .replace('DD', data['days'][date.getDay()])
+    .replace(/DD/, data['days'][date.getDay()])
     .replace(/D(?!e)/, data['days'][date.getDay()].substring(0, 3))
-    .replace('yyyy', date.getFullYear())
-    .replace('YYYY', date.getFullYear())
-    .replace('yy', (date.getFullYear()+'').substring(2))
-    .replace('YY', (date.getFullYear()+'').substring(2))
-    .replace('HH', pad(date.getHours(), 2))
-    .replace('H', date.getHours());
+    .replace(/yyyy/i, date.getFullYear())
+    .replace(/yy/i, (date.getFullYear()+'').substring(2))
+    .replace(/HH/, pad(date.getHours(), 2))
+    .replace(/H/, date.getHours());
 }
 
 function pad(text, length) {
