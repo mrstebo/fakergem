957602774e7c957224ee8ecc6e66da2fa35fa556
Steven Atkinson <mrstebouk@gmail.com>
2017-04-25 19:41:19 +0100

    Refactored arrays.

--- src/utils/date-helper.js
+++ src/utils/date-helper.js
@@ -1,13 +1,26 @@
 const DAYS = [
-  'Sunday', 'Monday', 'Tuesday', 'Wednesday',
-  'Thursday', 'Friday', 'Saturday'
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
 ];
 
 const MONTHS = [
-  'January', 'February', 'March',
-  'April', 'May', 'June', 'July',
-  'August', 'September', 'October',
-  'November', 'December'
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
 ];
 
 export function format(date, format) {
