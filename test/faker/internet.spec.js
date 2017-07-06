75b48e670f59bff32d072959964d228b4c591feb
Steven Atkinson <mrstebouk@gmail.com>
2017-07-06 23:27:09 +0100

    Cleaned up tests that have not been implemented yet.

--- test/faker/internet.spec.js
+++ test/faker/internet.spec.js
@@ -6,35 +6,15 @@ const nameData = require('../../data/name.json');
 
 describe('Internet', () => {
   describe('#email', () => {
-    it('should return a email', () => {
-      [...Array(100).keys()].forEach(_ => {
-        // expect(Internet.email()).to.be.oneOf(data['emails']);
-      });
-    });
   });
 
   describe('#freeEmail', () => {
-    it('should return a freeEmail', () => {
-      [...Array(100).keys()].forEach(_ => {
-        // expect(Internet.freeEmail()).to.be.oneOf(data['freeEmails']);
-      });
-    });
   });
 
   describe('#safeEmail', () => {
-    it('should return a safeEmail', () => {
-      [...Array(100).keys()].forEach(_ => {
-        // expect(Internet.safeEmail()).to.be.oneOf(data['safeEmails']);
-      });
-    });
   });
 
   describe('#userName', () => {
-    it('should return a userName', () => {
-      [...Array(100).keys()].forEach(_ => {
-        // expect(Internet.userName()).to.be.oneOf(data['userNames']);
-      });
-    });
   });
 
   describe('#password', () => {
@@ -104,74 +84,29 @@ describe('Internet', () => {
   });
 
   describe('#ipV4_address', () => {
-    it('should return a ipV4_address', () => {
-      [...Array(100).keys()].forEach(_ => {
-        // expect(Internet.ipV4_address()).to.be.oneOf(data['ipV4_addresses']);
-      });
-    });
   });
 
   describe('#privateIpV4_address', () => {
-    it('should return a privateIpV4_address', () => {
-      [...Array(100).keys()].forEach(_ => {
-        // expect(Internet.privateIpV4_address()).to.be.oneOf(data['privateIpV4_addresses']);
-      });
-    });
   });
 
   describe('#publicIpV4_address', () => {
-    it('should return a publicIpV4_address', () => {
-      [...Array(100).keys()].forEach(_ => {
-        // expect(Internet.publicIpV4_address()).to.be.oneOf(data['publicIpV4_addresses']);
-      });
-    });
   });
 
   describe('#ipV4_cidr', () => {
-    it('should return a ipV4_cidr', () => {
-      [...Array(100).keys()].forEach(_ => {
-        // expect(Internet.ipV4_cidr()).to.be.oneOf(data['ipV4_cidrs']);
-      });
-    });
   });
 
   describe('#ipV6_address', () => {
-    it('should return a ipV6_address', () => {
-      [...Array(100).keys()].forEach(_ => {
-        // expect(Internet.ipV6_address()).to.be.oneOf(data['ipV6_addresses']);
-      });
-    });
   });
 
   describe('#ipV6_cidr', () => {
-    it('should return a ipV6_cidr', () => {
-      [...Array(100).keys()].forEach(_ => {
-        // expect(Internet.ipV6_cidr()).to.be.oneOf(data['ipV6_cidrs']);
-      });
-    });
   });
 
   describe('#macAddress', () => {
-    it('should return a macAddress', () => {
-      [...Array(100).keys()].forEach(_ => {
-        // expect(Internet.macAddress()).to.be.oneOf(data['macAddresses']);
-      });
-    });
   });
 
   describe('#url', () => {
-    it('should return a url', () => {
-      [...Array(100).keys()].forEach(_ => {
-        // expect(Internet.url()).to.be.oneOf(data['urls']);
-      });
-    });
   });
 
   describe('#slug', () => {
-    it('should return a slug', () => {
-      [...Array(100).keys()].forEach(_ => {
-        // expect(Internet.slug()).to.be.oneOf(data['slugs']);
-      });
-    });
   });
 });
