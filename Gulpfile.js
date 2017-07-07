a64efe358a0272c3e1d1cfb7aaeeb3e18e0d49dc
Steven Atkinson <mrstebouk@gmail.com>
2017-07-07 09:32:21 +0100

    Removed the error handler (was causing travis to pass even if we had a failed test).

--- Gulpfile.js
+++ Gulpfile.js
@@ -40,12 +40,8 @@ gulp.task('test', ['istanbul'], () => {
       colors: true,
       harmony: true
     }))
-    .on('error', function(err) {
-      gutil.log(gutil.colors.red(err));
-      this.emit('end');
-    })
-    .pipe(istanbul.writeReports())
     .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }));
+    // .pipe(istanbul.writeReports())
 });
 
 gulp.task('coveralls', ['istanbul', 'test'], () => {
