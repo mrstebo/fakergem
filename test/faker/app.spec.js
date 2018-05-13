8eeb88f4748b40dcdbe69469e1826c0c8d98fc3f
Steven Atkinson <steven.atkinson@gmx.com>
2018-05-13 22:56:30 +0100

    Renamed data variable.

--- test/faker/app.spec.js
+++ test/faker/app.spec.js
@@ -3,19 +3,19 @@ const expect = require('chai').expect;
 const sinon = require('sinon');
 const sinonTest = require('sinon-test')(sinon, {useFakeTimers: false});
 const Faker = require('../../src/faker');
-const appData = require('../../data/app.json');
+const data = require('../../data/app.json');
 
 describe('App', () => {
   describe('#name', () => {
     it('returns a name', sinonTest(function() {
-      this.stub(Faker.Random, 'element').withArgs(appData['names']).returns('x');
+      this.stub(Faker.Random, 'element').withArgs(data['names']).returns('x');
       expect(Faker.App.name()).to.eql('x');
     }));
   });
 
   describe('#version', () => {
     it('returns a version format', sinonTest(function() {
-      this.stub(Faker.Random, 'element').withArgs(appData['versions']).returns('#.#.#.#');
+      this.stub(Faker.Random, 'element').withArgs(data['versions']).returns('#.#.#.#');
       this.stub(Faker.Number, 'between').withArgs(0, 9).returns(1);
       expect(Faker.App.version()).to.eql('1.1.1.1');
     }));
