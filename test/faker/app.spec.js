ac13337a789f7e155c5ac3a23363295339248412
Steven Atkinson <steven.atkinson@gmx.com>
2018-05-13 22:55:57 +0100

    Removed name data from App faker.

--- test/faker/app.spec.js
+++ test/faker/app.spec.js
@@ -4,35 +4,27 @@ const sinon = require('sinon');
 const sinonTest = require('sinon-test')(sinon, {useFakeTimers: false});
 const Faker = require('../../src/faker');
 const appData = require('../../data/app.json');
-const nameData = require('../../data/name.json');
 
 describe('App', () => {
   describe('#name', () => {
     it('returns a name', sinonTest(function() {
-      this.stub(Faker.Random, 'element')
-        .withArgs(appData['names'])
-        .returns('x');
+      this.stub(Faker.Random, 'element').withArgs(appData['names']).returns('x');
       expect(Faker.App.name()).to.eql('x');
     }));
   });
 
   describe('#version', () => {
     it('returns a version format', sinonTest(function() {
-      this.stub(Faker.Random, 'element')
-        .withArgs(appData['versions'])
-        .returns('#.#.#.#');
-      this.stub(Faker.Number, 'between')
-        .withArgs(0, 9)
-        .returns(1);
+      this.stub(Faker.Random, 'element').withArgs(appData['versions']).returns('#.#.#.#');
+      this.stub(Faker.Number, 'between').withArgs(0, 9).returns(1);
       expect(Faker.App.version()).to.eql('1.1.1.1');
     }));
   });
 
   describe('#author', () => {
     it('returns a name', sinonTest(function() {
-      const stub = this.stub(Faker.Random, 'element');
-      stub.withArgs(nameData['firstNames']).returns('John');
-      stub.withArgs(nameData['lastNames']).returns('Smith');
+      this.stub(Faker.Name, 'firstName').returns('John');
+      this.stub(Faker.Name, 'lastName').returns('Smith');
       expect(Faker.App.author()).to.eql('John Smith');
     }));
   });
