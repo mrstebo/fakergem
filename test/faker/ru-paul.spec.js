8306bd86654129892419d3affd204ab079ed0c24
Steve Atkinson <steve.atkinson@dootrix.com>
2018-05-19 10:40:31 +0100

    Updated the RuPaul faker tests.

--- test/faker/ru-paul.spec.js
+++ test/faker/ru-paul.spec.js
@@ -1,14 +1,15 @@
 'use strict';
 const expect = require('chai').expect;
+const sinon = require('sinon');
+const sinonTest = require('sinon-test')(sinon, {useFakeTimers: false});
 const Faker = require('../../src/faker');
 const data = require('../../data/ru-paul.json');
 
 describe('RuPaul', () => {
   describe('#quote', () => {
-    it('should return a quote', () => {
-      [...Array(100).keys()].forEach(_ => {
-        expect(Faker.RuPaul.quote()).to.be.oneOf(data['quotes']);
-      });
-    });
+    it('should return a quote', sinonTest(function() {
+      this.stub(Faker.Random, 'element').withArgs(data['quotes']).returns('quote');
+      expect(Faker.RuPaul.quote()).to.eql('quote');
+    }));
   });
 });
