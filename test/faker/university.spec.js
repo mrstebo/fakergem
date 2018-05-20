f36979800c2b70eeeaed524c2ae22b8ce01a19d6
Steven Atkinson <steven.atkinson@gmx.com>
2018-05-20 22:05:35 +0100

    Updated the University faker tests.

--- test/faker/university.spec.js
+++ test/faker/university.spec.js
@@ -1,14 +1,15 @@
 'use strict';
 const expect = require('chai').expect;
+const sinon = require('sinon');
+const sinonTest = require('sinon-test')(sinon, {useFakeTimers: false});
 const Faker = require('../../src/faker');
 const data = require('../../data/university.json');
 
 describe('University', () => {
   describe('#name', () => {
-    it('should return a name', () => {
-      [...Array(100).keys()].forEach(_ => {
-        expect(Faker.University.name()).to.be.oneOf(data['names']);
-      });
-    });
+    it('should return a name', sinonTest(function() {
+      this.stub(Faker.Random, 'element').withArgs(data['names']).returns('name');
+      expect(Faker.University.name()).to.eql('name');
+    }));
   });
 });
