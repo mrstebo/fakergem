38e02e531a36f650a0cc75d64ce8641ff8fd15d8
Steven Atkinson <steven.atkinson@gmx.com>
2018-05-20 22:02:17 +0100

    Updated the Team faker tests.

--- test/faker/team.spec.js
+++ test/faker/team.spec.js
@@ -1,39 +1,36 @@
 'use strict';
 const expect = require('chai').expect;
+const sinon = require('sinon');
+const sinonTest = require('sinon-test')(sinon, {useFakeTimers: false});
 const Faker = require('../../src/faker');
 const data = require('../../data/team.json');
-const addressData = require('../../data/address.json');
 
 describe('Team', () => {
   describe('#creature', () => {
-    it('should return a creature', () => {
-      [...Array(100).keys()].forEach(_ => {
-        expect(Faker.Team.creature()).to.be.oneOf(data['creatures']);
-      });
-    });
+    it('should return a creature', sinonTest(function() {
+      this.stub(Faker.Random, 'element').withArgs(data['creatures']).returns('creature');
+      expect(Faker.Team.creature()).to.eql('creature');
+    }));
   });
 
   describe('#name', () => {
-    it('should return a name', () => {
-      [...Array(100).keys()].forEach(_ => {
-        expect(Faker.Team.name()).to.be.oneOf(data['names']);
-      });
-    });
+    it('should return a name', sinonTest(function() {
+      this.stub(Faker.Random, 'element').withArgs(data['names']).returns('name');
+      expect(Faker.Team.name()).to.eql('name');
+    }));
   });
 
   describe('#state', () => {
-    it('should return a state', () => {
-      [...Array(100).keys()].forEach(_ => {
-        expect(Faker.Team.state()).to.be.oneOf(addressData['states']);
-      });
-    });
+    it('should return a state', sinonTest(function() {
+      this.stub(Faker.Address, 'state').returns('state');
+      expect(Faker.Team.state()).to.eql('state');
+    }));
   });
 
   describe('#sport', () => {
-    it('should return a sport', () => {
-      [...Array(100).keys()].forEach(_ => {
-        expect(Faker.Team.sport()).to.be.oneOf(data['sports']);
-      });
-    });
+    it('should return a sport', sinonTest(function() {
+      this.stub(Faker.Random, 'element').withArgs(data['sports']).returns('sport');
+      expect(Faker.Team.sport()).to.eql('sport');
+    }));
   });
 });
