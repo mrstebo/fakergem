31221326e870c527c4360ab523bb768c017d057b
Steve Atkinson <steve.atkinson@dootrix.com>
2018-05-20 16:27:31 +0100

    Updated the StarWars faker tests.

--- test/faker/star-wars.spec.js
+++ test/faker/star-wars.spec.js
@@ -1,68 +1,61 @@
 'use strict';
 const expect = require('chai').expect;
+const sinon = require('sinon');
+const sinonTest = require('sinon-test')(sinon, {useFakeTimers: false});
 const Faker = require('../../src/faker');
 const data = require('../../data/star-wars.json');
 
 describe('StarWars', () => {
   describe('#character', () => {
-    it('should return a character', () => {
-      [...Array(100).keys()].forEach(_ => {
-        expect(Faker.StarWars.character()).to.be.oneOf(data['characters']);
-      });
-    });
+    it('should return a character', sinonTest(function() {
+      this.stub(Faker.Random, 'element').withArgs(data['characters']).returns('character');
+      expect(Faker.StarWars.character()).to.eql('character');
+    }));
   });
 
   describe('#droid', () => {
-    it('should return a droid', () => {
-      [...Array(100).keys()].forEach(_ => {
-        expect(Faker.StarWars.droid()).to.be.oneOf(data['droids']);
-      });
-    });
+    it('should return a droid', sinonTest(function() {
+      this.stub(Faker.Random, 'element').withArgs(data['droids']).returns('droid');
+      expect(Faker.StarWars.droid()).to.eql('droid');
+    }));
   });
 
   describe('#planet', () => {
-    it('should return a planet', () => {
-      [...Array(100).keys()].forEach(_ => {
-        expect(Faker.StarWars.planet()).to.be.oneOf(data['planets']);
-      });
-    });
+    it('should return a planet', sinonTest(function() {
+      this.stub(Faker.Random, 'element').withArgs(data['planets']).returns('planet');
+      expect(Faker.StarWars.planet()).to.eql('planet');
+    }));
   });
 
   describe('#quote', () => {
-    it('should return a quote', () => {
-      [...Array(100).keys()].forEach(_ => {
-        expect(Faker.StarWars.quote()).to.be.oneOf(data['quotes']);
-      });
-    });
+    it('should return a quote', sinonTest(function() {
+      this.stub(Faker.Random, 'element').withArgs(data['quotes']).returns('quote');
+      expect(Faker.StarWars.quote()).to.eql('quote');
+    }));
   });
 
   describe('#specie', () => {
-    it('should return a specie', () => {
-      [...Array(100).keys()].forEach(_ => {
-        expect(Faker.StarWars.specie()).to.be.oneOf(data['species']);
-      });
-    });
+    it('should return a specie', sinonTest(function() {
+      this.stub(Faker.Random, 'element').withArgs(data['species']).returns('specie');
+      expect(Faker.StarWars.specie()).to.eql('specie');
+    }));
   });
 
   describe('#vehicle', () => {
-    it('should return a vehicle', () => {
-      [...Array(100).keys()].forEach(_ => {
-        expect(Faker.StarWars.vehicle()).to.be.oneOf(data['vehicles']);
-      });
-    });
+    it('should return a vehicle', sinonTest(function() {
+      this.stub(Faker.Random, 'element').withArgs(data['vehicles']).returns('vehicle');
+      expect(Faker.StarWars.vehicle()).to.eql('vehicle');
+    }));
   });
 
   describe('#wookieSentence', () => {
-    it('should return wookie words', () => {
-      [...Array(100).keys()].forEach(_ => {
-        Faker.StarWars.wookieSentence()
-          .split(/(?:\s|[.?!])/) // remove endings
-          .filter(x => x)
-          .forEach(word => {
-            expect(word.toLowerCase()).to.be.oneOf(data['wookieWords']);
-          });
-      });
-    });
+    it('should return wookie words', sinonTest(function() {
+      const randomStub = this.stub(Faker.Random, 'element');
+      randomStub.withArgs(data['wookieWords']).returns('word');
+      randomStub.withArgs(['.', '?', '!']).returns('*');
+      this.stub(Faker.Number, 'between').withArgs(1, 10).returns(3);
+      expect(Faker.StarWars.wookieSentence()).to.eql('Word word word*');
+    }));
 
     it('should start with a capital letter', () => {
       expect(Faker.StarWars.wookieSentence()).to.match(/^[A-Z]/);
