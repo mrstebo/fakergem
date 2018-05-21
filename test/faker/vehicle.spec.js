16f6ad084b757259897907c49dc92dd651b3d848
Steven Atkinson <steven.atkinson@gmx.com>
2018-05-21 23:46:00 +0100

    Added Vehicle faker test for handling vehicle without a wmiExt.

--- test/faker/vehicle.spec.js
+++ test/faker/vehicle.spec.js
@@ -7,13 +7,22 @@ const data = require('../../data/vehicle.json');
 
 describe('Vehicle', () => {
   describe('#vin', () => {
-    it('should return a vin', sinonTest(function() {
+    it('should return a VIN number', sinonTest(function() {
       const vinDetails = data['manufactures'].map(x => ({wmi: x[1], wmiExt: x[2]}));
       const vinCharacters = '0123456789ABCDEFGHJKLMNPRSTUVWXYZ';
       const randomStub = this.stub(Faker.Random, 'element');
       randomStub.withArgs(vinDetails).returns({wmi: '1A9', wmiExt: '396'});
       randomStub.withArgs(vinCharacters).returns('0');
       expect(Faker.Vehicle.vin()).to.eql('1A939');
+    }));
+
+    it('should handle VIN details without a wmiExt', sinonTest(function() {
+      const vinDetails = data['manufactures'].map(x => ({wmi: x[1], wmiExt: x[2]}));
+      const vinCharacters = '0123456789ABCDEFGHJKLMNPRSTUVWXYZ';
+      const randomStub = this.stub(Faker.Random, 'element');
+      randomStub.withArgs(vinDetails).returns({wmi: '1A9'});
+      randomStub.withArgs(vinCharacters).returns('0');
+      expect(Faker.Vehicle.vin()).to.eql('1A9');
     }));
   });
 
