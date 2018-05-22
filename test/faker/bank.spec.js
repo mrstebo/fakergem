7937e12dfbd61f70230caf27eba45acdda049918
Steven Atkinson <steven.atkinson@gmx.com>
2018-05-22 19:26:54 +0100

    Added test for default parameter and FR bank country code.

--- test/faker/bank.spec.js
+++ test/faker/bank.spec.js
@@ -26,7 +26,14 @@ describe('Bank', () => {
       betweenStub.withArgs(10, 99).returns(15);
       betweenStub.withArgs(0, 25).returns(3);
       betweenStub.withArgs(0, 9).returns(1);
-      expect(Faker.Bank.iban('GB')).to.eql('GB15DDDD11111111111111');
+      expect(Faker.Bank.iban()).to.eql('GB15DDDD11111111111111');
+    }));
+
+    it('should return an IBAN for the specified bank country code', sinonTest(function() {
+      const betweenStub = this.stub(Faker.Number, 'between');
+      betweenStub.withArgs(10, 99).returns(15);
+      betweenStub.withArgs(0, 9).returns(1);
+      expect(Faker.Bank.iban('FR')).to.eql('FR151111111111111111111111111');
     }));
   });
 });
