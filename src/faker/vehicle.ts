ca2518c7f9d46098c55af7e5df7f0eebdbc71244
Steven Atkinson <mrstebouk@gmail.com>
2019-10-16 21:39:01 +0100

    Updated the Vehicle faker.

--- src/faker/vehicle.ts
+++ src/faker/vehicle.ts
@@ -1,46 +1,46 @@
 import { Faker } from '../faker';
 import data from '../../data/vehicle.json';
 
-const VIN_CHARS = '0123456789.ABCDEFGH..JKLMN.P.R..STUVWXYZ';
-const VIN_MAP = '0123456789X';
-const VIN_WEIGHTS = '8765432X098765432';
-
-function vinChecksum(buffer) {
-  return VIN_MAP[buffer.map((c, i) => calculateVinWeight(c, i)) % 11];
-}
-
-function calculateVinWeight(character, i) {
-  (VIN_CHARS.indexOf(character) % 10) * VIN_MAP.indexOf(VIN_WEIGHTS[i]);
-}
-
 export class Vehicle {
+  private VIN_CHARS: string = '0123456789.ABCDEFGH..JKLMN.P.R..STUVWXYZ';
+  private VIN_MAP: string = '0123456789X';
+  private VIN_WEIGHTS: string = '8765432X098765432';
+
   private faker: Faker;
 
   constructor(faker: Faker) {
     this.faker = faker;
   }
 
-  vin() {
+  vin(): string {
     const details = this.faker.Random.element(data.manufactures.map(x => ({ wmi: x[1], wmiExt: x[2] })));
-    const vin = [...Array(14).keys()]
-      .map(_ => this.faker.Random.element(VIN_CHARS.split('').filter(x => x != '.')))
+    const vin = Array(14)
+      .map(_ => this.faker.Random.element(this.VIN_CHARS.split('').filter(x => x != '.')))
       .join('');
 
     let buffer = `${details.wmi}${vin}`.split('');
 
     if (details.wmiExt) {
-      [...Array(2).keys()].map(i => {
+      Array(2).map(i => {
         buffer[i + 12] = details.wmiExt[i];
       });
     }
 
     buffer[10] = this.faker.Random.element(data.years);
-    buffer[8] = vinChecksum(buffer);
+    buffer[8] = this.vinChecksum(buffer);
 
     return buffer.join('');
   }
 
-  manufacture() {
+  manufacture(): string {
     return this.faker.Random.element(data.manufactures.map(x => x[0]));
+  }
+
+  private vinChecksum(buffer: Array<string>): string {
+    return this.VIN_MAP[<any>buffer.map((c, i) => this.calculateVinWeight(c, i)) % 11];
+  }
+
+  private calculateVinWeight(character: string, i: number): number {
+    return (this.VIN_CHARS.indexOf(character) % 10) * this.VIN_MAP.indexOf(this.VIN_WEIGHTS[i]);
   }
 }
