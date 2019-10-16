ec68d78a64aab2f9a7def217f2c0ba1382e2c8a4
Steven Atkinson <mrstebouk@gmail.com>
2019-10-16 21:39:21 +0100

    Updated the Company faker.

--- src/faker/company.ts
+++ src/faker/company.ts
@@ -1,45 +1,6 @@
 import { Faker } from '../faker';
 import data from '../../data/company.json';
-
-function parse(faker, format) {
-  const text = format.replace(/\{(\w+)\}/g, m => `{Company.${m.substring(1)}`);
-  return faker.Fake.f(text);
-}
-
-function luhnAlgorithm(number) {
-  let sum = 0;
-  `${number}`
-    .split('')
-    .map((x, i) => (i % 2 == 0 ? parseInt(x) * 2 : parseInt(x)))
-    .forEach(n => `${n}`.split('').forEach(c => (sum += c.charCodeAt(0))));
-  return sum % 10 == 0 ? 0 : (sum / 10 + 1) * 10 - sum;
-}
-
-function mod11(number) {
-  const weight = [2, 3, 4, 5, 6, 7, 2, 3, 4, 5, 6, 7, 2, 3, 4, 5, 6, 7];
-  const sum = `${number}`
-    .split('')
-    .reverse()
-    .reduce((acc, val, index) => {
-      return val.charCodeAt(0) * weight[index - 1];
-    });
-  const remainder = sum % 11;
-
-  return 11 - remainder;
-}
-
-function abnChecksum(abn) {
-  const weights = [10, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
-
-  return weights.reduce((acc, val, index) => {
-    return acc + val * abn[index - 1];
-  });
-}
-
-function leftPad(text, length, paddingCharacter) {
-  let padding = [...Array(length)].map(_ => paddingCharacter).join('');
-  return (padding + text).slice(-length);
-}
+import { leftPad } from '../utils/text-helpers';
 
 export class Company {
   private faker: Faker;
@@ -48,72 +9,107 @@ export class Company {
     this.faker = faker;
   }
 
-  name() {
-    return parse(this.faker, this.faker.Random.element(data.names));
+  name(): string {
+    return this.parse(this.faker.Random.element(data.names));
   }
 
-  suffix() {
+  suffix(): string {
     return this.faker.Random.element(data.suffixes);
   }
 
-  catchPhrase() {
-    return [...Array(3).keys()].map(i => this.faker.Random.element(data.buzzwords[i])).join(' ');
+  catchPhrase(): string {
+    return Array(3).map(i => this.faker.Random.element(data.buzzwords[i])).join(' ');
   }
 
-  buzzword() {
+  buzzword(): string {
     return this.faker.Random.element([...data.buzzwords[0], ...data.buzzwords[1], ...data.buzzwords[2]]);
   }
 
-  bs() {
-    return [...Array(3).keys()].map(i => this.faker.Random.element(data.bs[i])).join(' ');
+  bs(): string {
+    return Array(3).map(i => this.faker.Random.element(data.bs[i])).join(' ');
   }
 
-  ein() {
-    return [...Array(9).keys()]
+  ein(): string {
+    return Array(9)
       .map(_ => this.faker.Number.between(0, 9))
       .join('')
       .replace(/(\d{2})(\d{7})/, '$1-$2');
   }
 
-  dunsNumber() {
-    return [...Array(9).keys()]
+  dunsNumber(): string {
+    return Array(9)
       .map(_ => this.faker.Number.between(0, 9))
       .join('')
       .replace(/(\d{2})(\d{3})(\d{4})/, '$1-$2-$3');
   }
 
-  logo() {
+  logo(): string {
     const number = this.faker.Number.between(1, 14);
     return `https://pigment.github.io/fake-logos/logos/medium/color/${number}.png`;
   }
 
-  swedishOrganisationNumber() {
+  swedishOrganisationNumber(): string {
     const base = [
       this.faker.Number.between(1, 9),
       this.faker.Number.between(0, 9),
       this.faker.Number.between(2, 9),
       this.faker.Number.number(6),
     ].join('');
-    return `${base}${luhnAlgorithm(base)}`;
+    return `${base}${this.luhnAlgorithm(base)}`;
   }
 
-  norwegianOrganisationNumber() {
+  norwegianOrganisationNumber(): string {
     let mod11Check = null;
     let base = '';
     while (mod11Check == null) {
       base = [this.faker.Random.element([8, 9]), leftPad(this.faker.Number.between(0, 10000000), 7, '0')].join('');
-      mod11Check = mod11(base);
+      mod11Check = this.mod11(base);
     }
     return `${base}${mod11Check}`;
   }
 
-  australianBusinessNumber() {
+  australianBusinessNumber(): string {
     const base = leftPad(this.faker.Number.between(0, 1000000000), 9, '0');
     const abn = `00${base}`;
-    return `${99 - (abnChecksum(abn) % 89)}${base}`;
+    return `${99 - (this.abnChecksum(abn) % 89)}${base}`;
   }
 
-  profession() {
+  profession(): string {
     return this.faker.Random.element(data.professions);
+  }
+
+  private parse(format: string): string {
+    const text = format.replace(/\{(\w+)\}/g, m => `{Company.${m.substring(1)}`);
+    return this.faker.Fake.f(text);
+  }
+
+  private luhnAlgorithm(number: number): number {
+    let sum = 0;
+    `${number}`
+      .split('')
+      .map((x, i) => (i % 2 == 0 ? parseInt(x) * 2 : parseInt(x)))
+      .forEach(n => `${n}`.split('').forEach(c => (sum += c.charCodeAt(0))));
+    return sum % 10 == 0 ? 0 : (sum / 10 + 1) * 10 - sum;
+  }
+
+  private mod11(number: number): number {
+    const weight = [2, 3, 4, 5, 6, 7, 2, 3, 4, 5, 6, 7, 2, 3, 4, 5, 6, 7];
+    const sum = `${number}`
+      .split('')
+      .reverse()
+      .reduce((acc, val, index) => {
+        return acc = ''+val.charCodeAt(0) * weight[index - 1];
+      });
+    const remainder = parseInt(sum) % 11;
+
+    return 11 - remainder;
+  }
+
+  private abnChecksum(abn: Array<number>): number {
+    const weights = [10, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+
+    return weights.reduce((acc, val, index) => {
+      return acc + val * abn[index - 1];
+    });
   }
 }
