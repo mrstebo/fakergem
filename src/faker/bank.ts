292b037476738b37f9f2b2f7e988f31e3e73c9d0
Steven Atkinson <mrstebouk@gmail.com>
2019-10-16 20:24:25 +0100

    Updated the Bank faker.

--- src/faker/bank.ts
+++ src/faker/bank.ts
@@ -8,21 +8,21 @@ export class Bank {
     this.faker = faker;
   }
 
-  name() {
-    return this.faker.Random.element(data['names']);
+  name(): string {
+    return this.faker.Random.element(data.names);
   }
 
-  swiftBic() {
-    return this.faker.Random.element(data['swiftBics']);
+  swiftBic(): string {
+    return this.faker.Random.element(data.swiftBics);
   }
 
-  iban(bankCountryCode = 'GB') {
-    const details = data['ibanDetails'].find(x => x['bankCountryCode'] == bankCountryCode.toUpperCase());
-    const bcc = `${details['bankCountryCode']}${this.faker.Number.between(10, 99)}`;
-    const ilc = [...Array(parseInt(details['ibanLetterCode'])).keys()]
+  iban(bankCountryCode: string = 'GB'): string {
+    const details = data.ibanDetails.filter(x => x.bankCountryCode == bankCountryCode.toUpperCase())[0];
+    const bcc = `${details.bankCountryCode}${this.faker.Number.between(10, 99)}`;
+    const ilc = Array(details.ibanLetterCode)
       .map(_ => String.fromCharCode(65 + this.faker.Number.between(0, 25)))
       .join('');
-    const ib = [...Array(parseInt(details['ibanDigits'])).keys()].map(_ => this.faker.Number.between(0, 9)).join('');
+    const ib = Array(details.ibanDigits).map(_ => this.faker.Number.between(0, 9)).join('');
     return `${bcc}${ilc}${ib}`;
   }
 }
