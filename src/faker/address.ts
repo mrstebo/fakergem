bd17f9defe2a3e0e4167d2c25d0f862618a88263
Steven Atkinson <mrstebouk@gmail.com>
2019-10-16 20:14:20 +0100

    Updated the Address faker.

--- src/faker/address.ts
+++ src/faker/address.ts
@@ -1,11 +1,6 @@
 import { Faker } from '../faker';
 import data from '../../data/address.json';
 
-function parse(faker: Faker, format: string) {
-  const text = format.replace(/\{(\w+)\}/g, m => `{Address.${m.substring(1)}`).replace(/#/, faker.Number.between(1, 9));
-  return faker.Fake.f(text);
-}
-
 export class Address {
   private faker: Faker;
 
@@ -14,24 +9,24 @@ export class Address {
   }
 
   city(): string {
-    const format = this.faker.Random.element(data['cities']);
-    return parse(this.faker, format);
+    const format = this.faker.Random.element(data.cities);
+    return this.parse(this.faker, format);
   }
 
   streetName(): string {
-    const format = this.faker.Random.element(data['streetNames']);
-    return parse(this.faker, format);
+    const format = this.faker.Random.element(data.streetNames);
+    return this.parse(this.faker, format);
   }
 
   streetAddress(): string {
-    const format = this.faker.Random.element(data['streetAddresses']);
-    return parse(this.faker, format);
+    const format = this.faker.Random.element(data.streetAddresses);
+    return this.parse(this.faker, format);
   }
 
   secondaryAddress(): string {
-    const prefix = this.faker.Random.element(data['secondaryAddressPrefixes']);
+    const prefix = this.faker.Random.element(data.secondaryAddressPrefixes);
     const format = `${prefix} ###`;
-    return parse(this.faker, format);
+    return this.parse(this.faker, format);
   }
 
   buildingNumber(): string {
@@ -41,9 +36,9 @@ export class Address {
   zipCode(stateAbbreviation = ''): string {
     const format =
       stateAbbreviation == ''
-        ? this.faker.Random.element(data['postcodes'])
-        : data['postcodeByState'][stateAbbreviation];
-    return parse(this.faker, format);
+        ? this.faker.Random.element(data.postcodes)
+        : (data.postcodeByState as { [key: string]: any })[stateAbbreviation];
+    return this.parse(this.faker, format);
   }
 
   zip(stateAbbreviation = ''): string {
@@ -55,35 +50,35 @@ export class Address {
   }
 
   timeZone(): string {
-    return this.faker.Random.element(data['timeZones']);
+    return this.faker.Random.element(data.timeZones);
   }
 
   streetSuffix(): string {
-    return this.faker.Random.element(data['streetSuffixes']);
+    return this.faker.Random.element(data.streetSuffixes);
   }
 
   citySuffix(): string {
-    return this.faker.Random.element(data['citySuffixes']);
+    return this.faker.Random.element(data.citySuffixes);
   }
 
   cityPrefix(): string {
-    return this.faker.Random.element(data['cityPrefixes']);
+    return this.faker.Random.element(data.cityPrefixes);
   }
 
   state(): string {
-    return this.faker.Random.element(data['states']);
+    return this.faker.Random.element(data.states);
   }
 
   stateAbbr(): string {
-    return this.faker.Random.element(data['stateAbbreviations']);
+    return this.faker.Random.element(data.stateAbbreviations);
   }
 
   country(): string {
-    return this.faker.Random.element(data['countries']);
+    return this.faker.Random.element(data.countries);
   }
 
   countryCode(): string {
-    return this.faker.Random.element(data['countryCodes']);
+    return this.faker.Random.element(data.countryCodes);
   }
 
   latitude(): string {
@@ -92,5 +87,10 @@ export class Address {
 
   longitude(): string {
     return '' + (this.faker.Number.betweenF(0.0, 360.0) - 180);
+  }
+
+  private parse(faker: Faker, format: string): string {
+    const text = format.replace(/\{(\w+)\}/g, m => `{Address.${m.substring(1)}`).replace(/#/, faker.Number.between(1, 9));
+    return faker.Fake.f(text);
   }
 }
