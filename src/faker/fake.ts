fe80dfae6f32da28a6520d6f0d4393b03e4eaa7c
Steven Atkinson <mrstebouk@gmail.com>
2019-10-16 21:39:32 +0100

    Updated the Fake faker.

--- src/faker/fake.ts
+++ src/faker/fake.ts
@@ -1,21 +1,5 @@
 import { Faker } from '../faker';
 
-function parseModule(faker, match) {
-  const v = match.replace(/[{}]/g, '').split('.');
-  const module = v[0];
-  const method = v[1];
-
-  if (typeof faker[module] === 'undefined') {
-    throw new Error(`Invalid module: ${module}`);
-  }
-
-  if (typeof faker[module][method] === 'undefined') {
-    throw new Error(`Invalid method: ${module}.${method}`);
-  }
-
-  return faker[module][method]();
-}
-
 export class Fake {
   private faker: Faker;
 
@@ -23,13 +7,26 @@ export class Fake {
     this.faker = faker;
   }
 
-  f(str) {
-    if (typeof str !== 'string' || str.length === 0) {
-      throw new Error('A string must be specified');
+  f(str: string): string {
+    return str
+      .replace(/\{(\w+)\.(\w+)\}/g, m => this.parseModule(this.faker, m))
+      .replace(/#/g, _ => this.faker.Number.between(0, 9));
+  }
+
+  private parseModule(faker: Faker, match: string): string {
+    const v = match.replace(/[{}]/g, '').split('.');
+    const module = v[0];
+    const method = v[1];
+    const fakerDictionary = faker as { [key: string]: any };
+
+    if (typeof fakerDictionary[module] === 'undefined') {
+      throw new Error(`Invalid module: ${module}`);
     }
 
-    return str
-      .replace(/\{(\w+)\.(\w+)\}/g, m => parseModule(this.faker, m))
-      .replace(/#/g, _ => this.faker.Number.between(0, 9));
+    if (typeof fakerDictionary[module][method] === 'undefined') {
+      throw new Error(`Invalid method: ${module}.${method}`);
+    }
+
+    return fakerDictionary[module][method]();
   }
 }
