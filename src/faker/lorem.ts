a523f02a1aab531318e8ec9dbcc3cee12d1350fd
Steven Atkinson <mrstebouk@gmail.com>
2019-10-16 23:03:37 +0100

    Updated the lorem fakers.

--- src/faker/lorem.ts
+++ src/faker/lorem.ts
@@ -1,16 +1,10 @@
 import { Faker } from '../faker';
 import data from '../../data/lorem.json';
+import { capitalize } from '../utils/text-helpers';
+import { resolveNumber } from '../utils/number-helpers';
 
 // 0-9, a-z
-const CHARACTERS = [...Array(10).keys()].concat([...Array(26).keys()].map(i => String.fromCharCode(97 + i)));
-
-function resolveNumber(n) {
-  return Math.max(parseInt(n), 0);
-}
-
-function capitalize(text) {
-  return text.replace(/\b\w/, x => x.toUpperCase());
-}
+const CHARACTERS = Array(10).concat([...Array(26).map(i => String.fromCharCode(97 + i))]);
 
 export class Lorem {
   private faker: Faker;
@@ -19,43 +13,62 @@ export class Lorem {
     this.faker = faker;
   }
 
-  word() {
+  word(): string {
     return this.faker.Random.element(data.words);
   }
 
-  words(num = 3, supplemental = false) {
+  words(
+    num: number = 3,
+    supplemental: boolean = false
+  ): Array<string> {
     const wordList = supplemental ? [...data.words, data.supplemental] : data.words;
     const words = this.faker.Random.assortment(wordList, resolveNumber(num));
     return words;
   }
 
-  character() {
+  character(): string {
     return this.faker.Random.element(CHARACTERS);
   }
 
-  characters(charCount = 255) {
-    return [...Array(resolveNumber(charCount)).keys()].map(_ => this.character()).join('');
+  characters(
+    charCount: number = 255
+  ): Array<string> {
+    return Array(resolveNumber(charCount)).map(_ => this.character());
   }
 
-  sentence(wordCount = 4, supplemental = false, randomWordsToAdd = 6) {
+  sentence(
+    wordCount: number = 4,
+    supplemental: boolean = false,
+    randomWordsToAdd: number = 6
+  ): string {
     const text = capitalize(
       this.words(resolveNumber(wordCount) + this.faker.Number.between(0, randomWordsToAdd), supplemental).join(' '),
     );
     return text.length > 0 ? `${text}.` : text;
   }
 
-  sentences(sentenceCount = 3, supplemental = false) {
-    return [...Array(resolveNumber(sentenceCount)).keys()].map(_ => this.sentence(3, supplemental));
+  sentences(
+    sentenceCount: number = 3,
+    supplemental: boolean = false
+  ): Array<string> {
+    return Array(resolveNumber(sentenceCount)).map(_ => this.sentence(3, supplemental));
   }
 
-  paragraph(sentenceCount = 3, supplemental = false, randomSentencesToAdd = 3) {
+  paragraph(
+    sentenceCount: number = 3,
+    supplemental: boolean = false,
+    randomSentencesToAdd: number = 3
+  ): string {
     return this.sentences(
       resolveNumber(sentenceCount) + this.faker.Number.between(0, randomSentencesToAdd),
       supplemental,
     ).join(' ');
   }
 
-  paragraphs(paragraphCount = 3, supplemental = false) {
-    return [...Array(resolveNumber(paragraphCount)).keys()].map(_ => this.paragraph(3, supplemental));
+  paragraphs(
+    paragraphCount: number = 3,
+    supplemental: boolean = false
+  ): Array<string> {
+    return Array(resolveNumber(paragraphCount)).map(_ => this.paragraph(3, supplemental));
   }
 }
