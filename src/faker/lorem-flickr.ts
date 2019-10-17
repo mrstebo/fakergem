23a772b60f51d2216ab4dbadbbea85c5f0a9428a
Steven Atkinson <mrstebouk@gmail.com>
2019-10-17 06:20:28 +0100

    Updated the LoremFlickr faker.

--- src/faker/lorem-flickr.ts
+++ src/faker/lorem-flickr.ts
@@ -1,72 +1,95 @@
 import { Faker } from '../faker';
-const SUPPORTED_COLORIZATIONS = ['red', 'green', 'blue'];
-
-function buildUrl(size, format, searchTerms, matchAll) {
-  if (!isValidSize(size)) {
-    throw new Error('Size should be specified in format 300x300');
-  }
-
-  let url = 'http://loremflickr.com';
-  if (format) {
-    url += `/${format}`;
-  }
-  url += `/${size.replace('x', '/')}`;
-  if ((searchTerms || []).length > 0) {
-    url += `/${searchTerms.join(',')}`;
-  }
-  if (matchAll) {
-    url += '/all';
-  }
-  return url;
-}
-
-function hasSearchTerms(searchTerms) {
-  return (searchTerms || []).length > 0;
-}
-
-function isColorizationSupported(color) {
-  return SUPPORTED_COLORIZATIONS.indexOf(color) >= 0;
-}
-
-function isValidSize(size) {
-  return /^\d+x\d+$/.test(size);
-}
 
 export class LoremFlickr {
+  private SUPPORTED_COLORIZATIONS: Array<string> = ['red', 'green', 'blue'];
+
   private faker: Faker;
 
   constructor(faker: Faker) {
     this.faker = faker;
   }
 
-  image(size = '300x300', searchTerms = [], matchAll = false) {
-    return buildUrl(size, null, searchTerms, matchAll);
+  image(
+    size: string = '300x300',
+    searchTerms: Array<string> = [],
+    matchAll: boolean = false
+  ): string {
+    return this.buildUrl(size, null, searchTerms, matchAll);
   }
 
-  grayscaleImage(size = '300x300', searchTerms = ['all'], matchAll = false) {
-    if (!hasSearchTerms(searchTerms)) {
+  grayscaleImage(
+    size: string = '300x300',
+    searchTerms: Array<string> = ['all'],
+    matchAll: boolean = false
+  ): string {
+    if (!this.hasSearchTerms(searchTerms)) {
       throw new Error('Search terms must be specified for grayscale images');
     }
 
-    return buildUrl(size, 'g', searchTerms, matchAll);
+    return this.buildUrl(size, 'g', searchTerms, matchAll);
   }
 
-  pixelatedImage(size = '300x300', searchTerms = ['all'], matchAll = false) {
-    if (!hasSearchTerms(searchTerms)) {
+  pixelatedImage(
+    size: string = '300x300',
+    searchTerms: Array<string> = ['all'],
+    matchAll: boolean = false
+  ): string {
+    if (!this.hasSearchTerms(searchTerms)) {
       throw new Error('Search terms must be specified for pixelated images');
     }
 
-    return buildUrl(size, 'p', searchTerms, matchAll);
+    return this.buildUrl(size, 'p', searchTerms, matchAll);
   }
 
-  colorizedImage(size = '300x300', color = 'red', searchTerms = ['all'], matchAll = false) {
-    if (!hasSearchTerms(searchTerms)) {
+  colorizedImage(
+    size: string = '300x300',
+    color: string = 'red',
+    searchTerms: Array<string> = ['all'],
+    matchAll: boolean = false
+  ): string {
+    if (!this.hasSearchTerms(searchTerms)) {
       throw new Error('Search terms must be specified for colorized images');
     }
-    if (!isColorizationSupported(color)) {
-      throw new Error(`Supported colorizations are ${SUPPORTED_COLORIZATIONS.join(', ')}`);
+    if (!this.isColorizationSupported(color)) {
+      throw new Error(`Supported colorizations are ${this.SUPPORTED_COLORIZATIONS.join(', ')}`);
     }
 
-    return buildUrl(size, color, searchTerms, matchAll);
+    return this.buildUrl(size, color, searchTerms, matchAll);
+  }
+
+  private buildUrl(
+    size: string,
+    format: string | null,
+    searchTerms: Array<string>,
+    matchAll: boolean
+  ): string {
+    if (!this.isValidSize(size)) {
+      throw new Error('Size should be specified in format 300x300');
+    }
+
+    let url = 'http://loremflickr.com';
+    if (format) {
+      url += `/${format}`;
+    }
+    url += `/${size.replace('x', '/')}`;
+    if ((searchTerms || []).length > 0) {
+      url += `/${searchTerms.join(',')}`;
+    }
+    if (matchAll) {
+      url += '/all';
+    }
+    return url;
+  }
+
+  private hasSearchTerms(searchTerms: Array<string>): boolean {
+    return (searchTerms || []).length > 0;
+  }
+
+  private isColorizationSupported(color: string): boolean {
+    return this.SUPPORTED_COLORIZATIONS.indexOf(color) >= 0;
+  }
+
+  private isValidSize(size: string): boolean {
+    return /^\d+x\d+$/.test(size);
   }
 }
