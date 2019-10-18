e35e995a17421724b44ff65fdf36624614f9152a
Steven Atkinson <mrstebouk@gmail.com>
2019-10-18 22:09:10 +0100

    Fixed sinon-test declaration in the File faker tests.

--- test/faker/file.test.ts
+++ test/faker/file.test.ts
@@ -2,39 +2,40 @@
 import { expect } from 'chai';
 import Faker from '../../src/index';
 import sinon from 'sinon';
-const sinonTest = require('sinon-test')(sinon, {useFakeTimers: false});
+import sinonTest from 'sinon-test';
 import data from '../../src/data/file.json';
+const test = sinonTest(sinon, {useFakeTimers: false});
 
 describe('File', () => {
   describe('#extension', () => {
-    it('should return an extension', sinonTest(function(this: typeof sinon) {
+    it('should return an extension', test(function(this: typeof sinon) {
       this.stub(Faker.Random, 'element').withArgs(data['extensions']).returns('extension');
       expect(Faker.File.extension()).to.eql('extension');
     }));
   });
 
   describe('#mimeType', () => {
-    it('should return a mime type', sinonTest(function(this: typeof sinon) {
+    it('should return a mime type', test(function(this: typeof sinon) {
       this.stub(Faker.Random, 'element').withArgs(data['mimeTypes']).returns('MIME Type');
       expect(Faker.File.mimeType()).to.eql('MIME Type');
     }));
   });
 
   describe('#fileName', () => {
-    it('should return a filename', sinonTest(function(this: typeof sinon) {
+    it('should return a filename', test(function(this: typeof sinon) {
       this.stub(Faker.Lorem, 'words').withArgs(2).returns(['word1', 'word2']);
       this.stub(Faker.Lorem, 'word').returns('name');
       this.stub(Faker.File, 'extension').returns('extension');
       expect(Faker.File.fileName()).to.eql('word1-word2/name.extension');
     }));
 
-    it('should return a filename with specified directory', sinonTest(function(this: typeof sinon) {
+    it('should return a filename with specified directory', test(function(this: typeof sinon) {
       this.stub(Faker.Lorem, 'word').returns('name');
       this.stub(Faker.File, 'extension').returns('extension');
       expect(Faker.File.fileName('test')).to.eql('test/name.extension');
     }));
 
-    it('should return a filename with specified name', sinonTest(function(this: typeof sinon) {
+    it('should return a filename with specified name', test(function(this: typeof sinon) {
       this.stub(Faker.File, 'extension').returns('extension');
       expect(Faker.File.fileName('test', 'my-file')).to.eql('test/my-file.extension');
     }));
