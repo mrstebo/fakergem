e9433038d3e4e935099189614f076f79e377c7ab
Steve Atkinson <steve.atkinson@dootrix.com>
2019-10-18 08:27:25 +0100

    Fixed issues with the Lorem faker.
    Changed CHARACTERS to string.
    Used Math.max to ensure we don't have a range less than 0.

--- src/faker/lorem.ts
+++ src/faker/lorem.ts
@@ -1,14 +1,9 @@
 import { Faker } from '../faker';
 import data from '../data/lorem.json';
 import { capitalize } from '../utils/text-helpers';
-import { resolveNumber } from '../utils/number-helpers';
 
 // 0-9, a-z
-const CHARACTERS = Array(10).concat([
-  ...Array(26)
-    .fill(null)
-    .map((_, i) => String.fromCharCode(97 + i)),
-]);
+const CHARACTERS = "0123456789abcdefghijklmnopqrstuvwxyz";
 
 export class Lorem {
   private faker: Faker;
@@ -23,7 +18,7 @@ export class Lorem {
 
   words(num: number = 3, supplemental: boolean = false): Array<string> {
     const wordList = supplemental ? [...data.words, data.supplemental] : data.words;
-    const words = this.faker.Random.assortment(wordList, resolveNumber(num));
+    const words = this.faker.Random.assortment(wordList, Math.max(0, num));
     return words;
   }
 
@@ -32,34 +27,34 @@ export class Lorem {
   }
 
   characters(charCount: number = 255): string {
-    return Array(resolveNumber(charCount))
+    return Array(Math.max(0, charCount))
       .fill(null)
-      .map(_ => this.character())
+      .map(() => this.character())
       .join('');
   }
 
   sentence(wordCount: number = 4, supplemental: boolean = false, randomWordsToAdd: number = 6): string {
     const text = capitalize(
-      this.words(resolveNumber(wordCount) + this.faker.Number.between(0, randomWordsToAdd), supplemental).join(' '),
+      this.words(Math.max(0, wordCount) + this.faker.Number.between(0, Math.max(0, randomWordsToAdd)), supplemental).join(' '),
     );
     return text.length > 0 ? `${text}.` : text;
   }
 
   sentences(sentenceCount: number = 3, supplemental: boolean = false): Array<string> {
-    return Array(resolveNumber(sentenceCount))
+    return Array(Math.max(0, sentenceCount))
       .fill(null)
       .map(_ => this.sentence(3, supplemental));
   }
 
   paragraph(sentenceCount: number = 3, supplemental: boolean = false, randomSentencesToAdd: number = 3): string {
     return this.sentences(
-      resolveNumber(sentenceCount) + this.faker.Number.between(0, randomSentencesToAdd),
+      sentenceCount + this.faker.Number.between(0, randomSentencesToAdd),
       supplemental,
     ).join(' ');
   }
 
   paragraphs(paragraphCount: number = 3, supplemental: boolean = false): Array<string> {
-    return Array(resolveNumber(paragraphCount))
+    return Array(Math.max(0, paragraphCount))
       .fill(null)
       .map(_ => this.paragraph(3, supplemental));
   }
